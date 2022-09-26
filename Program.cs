f7855361c8aa8531bac8d751b93c498d2f93e85b
DontsovD <triggonayt@yandex.ru>
2022-09-26 15:37:02 +0300

    Task23

--- Program.cs
+++ Program.cs
@@ -30,25 +30,44 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.Clear();
-Console.WriteLine("Enter the X coordinates of point A");
-int a1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter the Y coordinates of point A");
-int a2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter the Z coordinates of point A");
-int a3 = Convert.ToInt32(Console.ReadLine());
+// Console.Clear();
+// Console.WriteLine("Enter the X coordinates of point A");
+// int a1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter the Y coordinates of point A");
+// int a2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter the Z coordinates of point A");
+// int a3 = Convert.ToInt32(Console.ReadLine());
 
 
-Console.WriteLine("Enter the X coordinates of point B");
-int b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter the Y coordinates of point B");
-int b2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter the Z coordinates of point B");
-int b3 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter the X coordinates of point B");
+// int b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter the Y coordinates of point B");
+// int b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter the Z coordinates of point B");
+// int b3 = Convert.ToInt32(Console.ReadLine());
 
-// √((x2-x1)^2 )+(y2-y1)^2+(z2-z1)^2
+// // √((x2-x1)^2 )+(y2-y1)^2+(z2-z1)^2
 
-double ab = Math.Sqrt(Math.Pow((a1-b1), 2) + Math.Pow((a2-b2), 2) + Math.Pow((a3-b3), 2));
+// double ab = Math.Sqrt(Math.Pow((a1-b1), 2) + Math.Pow((a2-b2), 2) + Math.Pow((a3-b3), 2));
 
-Console.WriteLine($" Distance between points = {ab}");
+// Console.WriteLine($" Distance between points = {ab}");
+
+
+
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125  
+
+Console.WriteLine("Enter number ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i < n+1)
+{
+    Console.Write($"{Math.Pow(i, 3)} "); i++;
+}
+
 
